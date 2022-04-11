55a28ece655fa34c9f296ad1523d1ca68760ea25
Sergey <poker4grig@yandex.ru>
2022-04-11 20:37:20 +0300

    add hash checking for task 4

--- meths_collecting_and_processing_data/lesson_4/task.py
+++ meths_collecting_and_processing_data/lesson_4/task.py
@@ -12,6 +12,8 @@ import requests
 from lxml import html
 import time
 from pymongo import MongoClient
+import hashlib
+import json
 from pprint import pprint
 from pymongo.errors import DuplicateKeyError
 
@@ -26,12 +28,21 @@ headers = {
 response = requests.get(main_url, headers=headers)
 
 # Получаем объект DOM
-time.sleep(5)
 dom = html.fromstring(response.text)
 
 all_news = list()
-top_one_news = dict()
+
+
+def hash_function(item: dict) -> str:
+    """Функция получения хэш."""
+
+    _hash = hashlib.md5(bytes(json.dumps(item, ensure_ascii=False),
+                              encoding='utf-8'))
+    return _hash.hexdigest()
+
+
 # Обрабатываем главную новость
+top_one_news = dict()
 top_one_container = dom.xpath('//div[@class="mg-card mg-card_type_image mg-card_stretching mg-card_flexible-double mg-grid__item"]')
 for con in top_one_container:
     time = date + ' ' + con.xpath('.//span[@class="mg-card-source__time"]/text()')[0]
@@ -42,6 +53,7 @@ for con in top_one_container:
     top_one_news['Название источника'] = source
     top_one_news['Название новости'] = name
     top_one_news['Ссылка на новость'] = link
+    top_one_news['_id'] = hash_function(top_one_news)
 if top_one_news:
     all_news.append(top_one_news)
 
@@ -58,13 +70,27 @@ for block in other_news_container:
         other_news['Название источника'] = source
         other_news['Название новости'] = name
         other_news['Ссылка на новость'] = link
+        other_news['_id'] = hash_function(other_news)
     all_news.append(other_news)
 # Записываем в базу данных
 yandex = db.yandex
-for news in all_news:
-    try:
-        yandex.insert_one(news)
-    except DuplicateKeyError:
-        print(f"Document with id = {news['_id']} already exist")
-# yandex.delete_many({})  # Очищение базы
-pprint(list(yandex.find({})))  # Показать все содержимое базы
+
+
+def mongo_save(database, data):
+    """Функция работы с БД."""
+
+    _yandex = database.yandex
+
+    for item in data:
+        yandex_hash = item['_id']
+        if not list(_yandex.find({'_id': yandex_hash})):
+            try:
+                _yandex.insert_one(item)
+            except DuplicateKeyError:
+                print(f"Document with id = {item['_id']} already exist")
+
+
+if __name__ == '__main__':
+    mongo_save(db, all_news)
+    # yandex.delete_many({})  # Очищение базы
+    pprint(list(yandex.find({})))  # Показать все содержимое базы
